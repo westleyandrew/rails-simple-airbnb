5c6db77325e207bb66d3bb77dd1b688b377409c4
Westley Andrew <westleyandrew@hotmail.com>
2019-11-07 15:30:40 +0000

    index page completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+Flat.destroy_all
+
 Flat.create!(
   name: 'Light & Spacious Garden Flat London',
   address: '10 Clifton Gardens London W9 1DT',
@@ -7,25 +9,17 @@ Flat.create!(
 )
 
 Flat.create!(
-  name: 'Lovely Townhouse Flat London',
-  address: '17 Fleet Street London EC4Y 1AA',
+  name: 'Stylish House Close to River Thames',
+  address: '5 Queensmill Road London SW6 6JP',
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
-  price_per_night: 90,
-  number_of_guests: 5
+  price_per_night: 65,
+  number_of_guests: 2
 )
 
 Flat.create!(
-  name: 'Light & Spacious Garden Flat London',
-  address: '17 Cranfield Road London SE4 1TN',
+  name: 'St Pancras Clock Tower Guest Suite',
+  address: 'Euston Rd London N1C 4QP',
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
-  price_per_night: 95,
-  number_of_guests: 3
-)
-
-Flat.create!(
-  name: 'Light & Spacious Garden Flat London',
-  address: '17 Breakspears Road, London SE4 1XW',
-  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
-  price_per_night: 50,
-  number_of_guests: 3
+  price_per_night: 110,
+  number_of_guests: 2
 )
